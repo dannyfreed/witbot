9e64a8869671327a2b1bc37e6898d27f19220e45
Danny Freed <freed.danny@gmail.com>
2016-01-21 23:01:32 -0600

    onboarding started

--- index.js
+++ index.js
@@ -33,9 +33,10 @@ controller.hears('.*', 'direct_message,direct_mention', function (bot, message) 
   witbot.process(message.text, bot, message)
 })
 
-witbot.hears('hello', 0.5, function (bot, message, outcome) {
-  bot.reply(message, 'Hello to you as well!')
-})
+// witbot.hears('hello', 0.5, function (bot, message, outcome) {
+//   bot.reply(message, 'Hello to you as well!')
+// })
+
 
 
 var weather = require('./weather')(openWeatherApiKey)
@@ -123,16 +124,91 @@ witbot.hears('performance', 0.5, function (bot, message, outcome) {
   })
 })
 
-function onboard(bot, message){
-  bot.say(
-    {
-      text: 'Hello. Beep Beoop bop boop. I am a robot! :simplesmile:',
-      channel: '#general'
-    }
-  );
-};
 
-onboard();
+controller.on('channel_joined',function(bot,message) {
+    bot.say(
+      {
+        text: "Hi, I am Guru. Say `@guru help` to see what I can do",
+        channel: message.channel.id
+      }
+    );
+});
+
+
+
+//ONLY WORKS WITH AMBIENT ON???
+controller.hears(['help'],['direct_message','direct_mention','mention','ambient'],function(bot,message) {
+
+  // start a conversation to handle this response.
+  bot.startConversation(message,function(err,convo) {
+
+    convo.say("Ready to become an analytics guru? :nerd_face:  Here's how I can help you.");
+    convo.say("To get started, you have to let me know what type of metric you're interested in learning more about. For example, `users`, `sessions`, `pageviews`, just to name a few.");
+    convo.say("To see a full list of supported metrics, type `@guru: list metrics`");
+
+
+
+  })
+
+});
+
+controller.hears(['list metrics'],['direct_message','direct_mention','mention','ambient'],function(bot,message) {
+
+	// var attachments = [];
+	//   var attachment = {
+	//     title: 'Users',
+	//     color: '#0099ff',
+	//     fields: [],
+	//   };
+
+	//   attachment.fields.push({
+	//     label: 'Users',
+	//     value: "`users` ```new users``` `existing users` `one-time users` ",
+	//     short: false,
+	//   });
+
+	  var attachments  = [
+        {
+            "title": "Users",
+            "text": "`users` `new users` `existing users` `one-time users`",
+
+            "mrkdwn_in": ["text"]
+        },
+        {
+            "title": "Sessions",
+            "text": "`sessions` `bounces` `bounce rate` `session duration` `avg session duration`",
+            "mrkdwn_in": ["text"]
+        }
+    ]
+
+
+
+
+	  //attachments.push(attachment);
+
+	  bot.reply(message,{
+	    text: 'Here are all the metrics I can currently pull...',
+	    attachments: attachments,
+	  },function(err,resp) {
+	    console.log(err,resp);
+	  });
+
+});
+
+
+
+
+// function onboard(){
+//   witbot.say(
+//     {
+//       text: 'Hello. Beep Beoop bop boop. I am a robot! :simplesmile:',
+//       channel: '#general'
+//     }
+//   );
+// };
+
+// onboard();
+
 
 
 
