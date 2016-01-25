9cd794620f3958ca124e791d28a43004db4c18c8
Danny Freed <freed.danny@gmail.com>
2016-01-24 18:20:44 -0600

    for beep boop

--- analytics.js
+++ analytics.js
@@ -17,8 +17,8 @@ var SERVICE_ACCOUNT_EMAIL = process.env.SERVICE_ACCOUNT_EMAIL
 var SERVICE_ACCOUNT_KEY_FILE = path.join(os.tmpdir(), crypto.randomBytes(Math.ceil(6)).toString('hex').slice(0,12))
 console.log("SERVICE_ACCOUNT_KEY_FILE=" + SERVICE_ACCOUNT_KEY_FILE)
 fs.writeFileSync(SERVICE_ACCOUNT_KEY_FILE, new Buffer(process.env.KEY_PEM, 'base64'), { mode: '600' }, 'utf8')
-// process.on('exit', fs.unlinkSync.bind(null, SERVICE_ACCOUNT_KEY_FILE))
-// process.on('SIGINT', fs.unlinkSync.bind(null, SERVICE_ACCOUNT_KEY_FILE))
+process.on('exit', fs.unlinkSync.bind(null, SERVICE_ACCOUNT_KEY_FILE))
+process.on('SIGINT', fs.unlinkSync.bind(null, SERVICE_ACCOUNT_KEY_FILE))
 
 var SERVICE_ACCOUNT_EMAIL = process.env.SERVICE_ACCOUNT_EMAIL
 //var SERVICE_ACCOUNT_KEY_FILE = __dirname + process.env.KEY_PEM
